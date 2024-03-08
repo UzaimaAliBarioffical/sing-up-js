3ca17e257c903d755572ac8a26cee4be7f9a5739
UzaimaAliBarioffical <156655094+UzaimaAliBarioffical@users.noreply.github.com>
2024-03-08 11:57:35 -0800

    work

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var userName = prompt(" SingUp (Yes/No)")
+
+if (userName == "yes") {
+    prompt("Enter Your Name!") &&
+        alert("SignUp Succeesfully")
+} else {
+    alert("Go Back")
+}
+Array [""]
+console.log
